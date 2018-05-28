05ac80e55b393895d8d97525d4d00d89e02bd4b9
yoginayak <38529406+yoginayak@users.noreply.github.com>
2018-05-28 02:57:06 -0700

    Add files via upload

---
+++ e13.js
@@ -0,0 +1,14 @@
+var express=require('express');
+var cookieParser=require('cookie-parser');
+var app=express();
+app.use(cookieParser());
+app.get('/',function(req,res)
+	{
+		res.cookie('Test','Cookie');
+		res.send('cookie is set');
+	});
+app.get('/get',function(req,res)
+	{
+		console.log(req.cookies);
+	});
+app.listen(8080);
